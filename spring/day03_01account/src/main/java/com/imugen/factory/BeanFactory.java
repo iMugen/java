7f0d4c5983b0864052234e5004da720070b81b7d
iMugen <liu1142865396@gmail.com>
2020-03-22 15:44:17 +0800

    update

--- spring/day03_01account/src/main/java/com/imugen/factory/BeanFactory.java
+++ spring/day03_01account/src/main/java/com/imugen/factory/BeanFactory.java
@@ -20,7 +20,6 @@ public class BeanFactory {
         this.txManager = txManager;
     }
 
-
     public final void setAccountService(IAccountService accountService) {
         this.accountService = accountService;
     }
