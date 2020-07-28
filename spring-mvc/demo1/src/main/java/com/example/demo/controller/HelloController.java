972f4db02f4a37cc5c69411a79839d83b1ccbcb7
liuqiang <liu1142865396@gmail.com>
2020-07-28 21:34:07 +0800

    update：添加spring-boot-demo用于测试接口

---
+++ spring-mvc/demo1/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * Hello Controller
+ * </p>
+ *
+ * @package: com.xkcoding.docker.controller
+ * @description: Hello Controller
+ * @author: yangkai.shen
+ * @date: Created in 2018-11-29 14:58
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+@RestController
+@RequestMapping
+public class HelloController {
+    @GetMapping
+    public String hello() {
+        return "Hello,From Docker!";
+    }
+
+    @GetMapping("/num")
+    public int getNum(){
+        return 3000;
+    }
+}
