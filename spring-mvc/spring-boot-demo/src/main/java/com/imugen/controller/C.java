21bc8e614216e529d02954abd106d84d3ad2549f
liuqiang <liu1142865396@gmail.com>
2020-10-28 14:04:04 +0800

    update:nothing

---
+++ spring-mvc/spring-boot-demo/src/main/java/com/imugen/controller/C.java
@@ -0,0 +1,13 @@
+package com.imugen.controller;
+
+/**
+ * @author Daniel
+ * @date 2020/10/28
+ **/
+public class C {
+
+    public static void main(String[] args) {
+        System.out.println("hhh");
+    }
+
+}
